362f752593d9d0012113add1254a0af1423c9c48
Hazim Razak <83561905+mhazimaqlan@users.noreply.github.com>
2022-01-13 10:40:21 +0800

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+// This is a simple Java program.
+// FileName : "HelloWorld.java".
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!!");
+    }
+}
